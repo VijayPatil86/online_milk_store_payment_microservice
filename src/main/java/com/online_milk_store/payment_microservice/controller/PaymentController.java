3df6ddbcb64bbb619f5b9a75d301c67a56f29665
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-05 13:07:22 +0530

    initial commit

---
+++ src/main/java/com/online_milk_store/payment_microservice/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package com.online_milk_store.payment_microservice.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.online_milk_store.payment_microservice.bean.PaymentDetails;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/payment-service/payment")
+public class PaymentController {
+
+	static final private Logger LOGGER = LogManager.getLogger(PaymentController.class);
+
+	@PostMapping
+	public ResponseEntity<Void> processPayment(@RequestBody PaymentDetails paymentDetails) {
+		LOGGER.debug("PaymentController.processPayment() --- START");
+		LOGGER.info("PaymentController.processPayment() --- paymentDetails: " + paymentDetails);
+		LOGGER.debug("PaymentController.processPayment() --- END");
+		return null;
+	}
+}
