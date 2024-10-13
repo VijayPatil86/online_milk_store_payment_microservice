f9e8cba4719cdc8d1076d60bcd6a65fc8f9f8bc0
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-13 20:15:37 +0530

    initial commit

---
+++ src/main/java/com/online_milk_store/payment_microservice/entity/UPIPaymentTransactionEntity.java
@@ -0,0 +1,50 @@
+package com.online_milk_store.payment_microservice.entity;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @Builder @NoArgsConstructor @AllArgsConstructor @ToString
+@Entity
+@Table(name = "PAYMENT_TRANSACTION")
+public class UPIPaymentTransactionEntity {
+	@Column(name = "TRANSACTION_ID")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int transactionId;
+
+	@Column(name = "ORDER_NUMBER")
+	private String orderNumber;
+
+	@Column(name = "PAYMENT_METHOD")
+	private String paymentMethod;
+
+	@Column(name = "PAYMENT_STATUS")
+	private String paymentStatus;
+
+	@Column(name = "CUSTOMER_BANK_TXN_ID")
+	private String custBankTxnId;
+
+	@Column(name = "CREATED_AT")
+	private Timestamp createdAt;
+
+	@ManyToOne
+	@JoinColumn(name = "UPI_RECORD_ID")
+	private UPIDetailsEntity upiDetailsEntity;
+
+	@Column(name = "REMARK")
+	private String upiPaymentRemark;
+}
