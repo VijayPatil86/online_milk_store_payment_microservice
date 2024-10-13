5c4b9e16969ea2790876a5e8c1f4248ea073c537
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-13 20:11:40 +0530

    initial commit

---
+++ src/main/java/com/online_milk_store/payment_microservice/bean/UPIPaymentTransactionBean.java
@@ -0,0 +1,17 @@
+package com.online_milk_store.payment_microservice.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @Builder @NoArgsConstructor @AllArgsConstructor @ToString
+public class UPIPaymentTransactionBean {
+	private String orderNumber;
+	private String upiId;
+	private String upiPaymentMethod;
+	private String upiPaymentStatus;
+	private String upiPaymentRemark;
+}
