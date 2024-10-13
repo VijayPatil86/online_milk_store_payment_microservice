75c14d5de0ab9790750effb1077763b41fd96aaf
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-13 20:20:32 +0530

    removed

--- src/main/java/com/online_milk_store/payment_microservice/payment/PaymentProcessor.java
+++
@@ -1,7 +0,0 @@
-package com.online_milk_store.payment_microservice.payment;
-
-import com.online_milk_store.payment_microservice.bean.PaymentDetails;
-
-public interface PaymentProcessor {
-	void processPayment(PaymentDetails paymentDetails);
-}
