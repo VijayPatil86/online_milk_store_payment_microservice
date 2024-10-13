1bc38395c306a8fd80443747d6b81be6e269a488
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-13 20:13:55 +0530

    added code to handle UPI payment request

--- src/main/java/com/online_milk_store/payment_microservice/controller/PaymentController.java
+++ src/main/java/com/online_milk_store/payment_microservice/controller/PaymentController.java
@@ -2,27 +2,31 @@ package com.online_milk_store.payment_microservice.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.online_milk_store.payment_microservice.bean.PaymentDetails;
+import com.online_milk_store.payment_microservice.bean.UPIPaymentTransactionBean;
+import com.online_milk_store.payment_microservice.service.PaymentService;
 
-@CrossOrigin
 @RestController
-@RequestMapping("/payment-service/payment")
+@RequestMapping(path = "/payment-service")
 public class PaymentController {
 
 	static final private Logger LOGGER = LogManager.getLogger(PaymentController.class);
 
-	@PostMapping
-	public ResponseEntity<Void> processPayment(@RequestBody PaymentDetails paymentDetails) {
-		LOGGER.debug("PaymentController.processPayment() --- START");
-		LOGGER.info("PaymentController.processPayment() --- paymentDetails: " + paymentDetails);
-		LOGGER.debug("PaymentController.processPayment() --- END");
+	@Autowired
+	private PaymentService paymentService;
+
+	@PostMapping(path = "/upi-payment")
+	public ResponseEntity<Void> processUPIPayment(@RequestBody UPIPaymentTransactionBean upiPaymentTransactionBean) {
+		LOGGER.debug("PaymentController.processUPIPayment() --- START");
+		LOGGER.info("PaymentController.processUPIPayment() --- upiPaymentTransactionBean: " + upiPaymentTransactionBean);
+		paymentService.processUPIPayment(upiPaymentTransactionBean);
+		LOGGER.debug("PaymentController.processUPIPayment() --- END");
 		return null;
 	}
 }
