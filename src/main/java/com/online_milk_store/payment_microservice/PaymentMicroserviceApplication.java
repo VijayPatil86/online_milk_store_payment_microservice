85079d4aa996d6be74a0a054c095613923f357dc
VijayPatil86 <vijipatil090386@gmail.com>
2024-10-16 17:09:19 +0530

    added annotation @EnableFeignClients

--- src/main/java/com/online_milk_store/payment_microservice/PaymentMicroserviceApplication.java
+++ src/main/java/com/online_milk_store/payment_microservice/PaymentMicroserviceApplication.java
@@ -2,7 +2,9 @@ package com.online_milk_store.payment_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class PaymentMicroserviceApplication {
 
